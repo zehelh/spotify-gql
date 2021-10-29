92d83bdfb2d781265cd11199e8554831b60fe758
zehelh <yvan.brai@gmail.com>
2021-10-29 12:06:32 +0200

    add elementUI

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import router from './router'
 import store from './store'
 import ApolloClient from './apollo/index'
 import { createApolloProvider } from '@vue/apollo-option'
+import ElementUI from 'element-ui'
 
 const apolloProvider = createApolloProvider({
   defaultClient: ApolloClient
 })
 
-createApp(App).use(store).use(router).use(apolloProvider).mount('#app')
+createApp(App).use(ElementUI).use(store).use(router).use(apolloProvider).mount('#app')
