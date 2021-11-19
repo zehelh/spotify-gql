67f8ba772c993799dde1f2fb5e3792d4228cb61d
zehelh <yvan.brai@gmail.com>
2021-11-19 11:40:15 +0100

    del vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import ApolloClient from './apollo/index'
 import { createApolloProvider } from '@vue/apollo-option'
 
@@ -12,4 +11,4 @@ const apolloProvider = createApolloProvider({
     defaultClient: ApolloClient,
 })
 
-createApp(App).use(store).use(router).use(apolloProvider).mount('#app')
+createApp(App).use(router).use(apolloProvider).mount('#app')
