7a0f126f0526ce374d39abe130eeb2b0719c44d9
Antho <anthony.34.lopez@gmail.com>
2021-11-19 10:08:09 +0100

    Finalisation de la partie Missions
    Code fonctionnel de la liste des missions et du détail d'une mission sélectionnée

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/SpaceX-Logo.svg/1280px-SpaceX-Logo.svg.png" alt=""></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/missions">missions</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <router-view/>
 </template>
 
@@ -26,5 +40,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.logo{
+  height: 20px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>{{viewer.login}}</p>
-
-    <div class="card-style" v-for="repo in viewer.repositories.nodes" :key="repo.id">
-      <p>{{repo.name}}</p>
-    </div>
+    
   </div>
 </template> 
 
@@ -19,36 +14,6 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
-  },
-
-  apollo: {
-    viewer: gql`query { 
-      viewer { 
-        login
-        repositories(first: 10) {
-          nodes{
-            id
-            name
-          }
-        }
-      }
-    }`,
-
-    
-  },
+  }
 }
 </script>
-<style scoped>
-
-.card-style{
-  width: 80%;
-  margin-left: 10%;
-  border: 1px solid rgb(139, 139, 139);
-  -webkit-box-shadow: 5px 5px 10px 5px rgba(0,0,0,0.55); 
-  box-shadow: 5px 5px 10px 5px rgba(0,0,0,0.55);
-  margin-bottom: 20px;
-  border-radius: 4px;
-
-}
-
-</style>
